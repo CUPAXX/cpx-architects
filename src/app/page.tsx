74dc8c1b5189aeb6af1482ee52e360bc8dc90ab1
CUPAXXHD <fiqri.fa42@gmail.com>
2024-09-23 23:57:23 +0700

    add main banner component, add public assets, and create hooks useIntervals

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,13 @@
+"use client";
+
 import { Navbar } from "@components/Navbar/Navbar";
-import Image from "next/image";
-import mainBanner from "@assets/main-banner.jpg";
+import { MainBanner } from "@components/MainBanner/MainBanner";
 
 export default function Home() {
   return (
     <div>
       <Navbar />
-      <section className="relative w-full h-[41rem]">
-        <Image src={mainBanner} fill sizes="100vw" alt="main banner" />
-      </section>
+      <MainBanner />
     </div>
   );
 }
---
+++ src/components/MainBanner/MainBanner.tsx
@@ -0,0 +1,38 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+"use client";
+
+import React, { useState } from "react";
+import Image from "next/image";
+import { useInterval } from "@hooks/useInterval";
+import { bannerMaps } from "@maps/banner_maps";
+
+export const MainBanner = () => {
+  const [activeId, activeIdSet] = useState(0);
+
+  useInterval(() => {
+    activeIdSet(activeId === bannerMaps.length - 1 ? 0 : activeId + 1);
+  }, 3000);
+
+  return (
+    <div className="w-full h-[41rem] relative">
+      <div className="bg-white absolute h-full w-full z-20 opacity-20" />
+      {bannerMaps.map((res) => {
+        if (res.id === activeId) {
+          return (
+            <Image
+              key={res.id}
+              src={res.path}
+              fill
+              sizes="100vw"
+              alt={res.label}
+              placeholder="blur"
+              blurDataURL={res.blurData}
+              quality={80}
+              className="object-cover relative z-10"
+            />
+          );
+        }
+      })}
+    </div>
+  );
+};
